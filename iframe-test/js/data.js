aac9833628f50e231c93142759eb920931355f16
Lars Lund <larslund@Larss-MacBook-Pro.local>
2013-10-22 14:35:39 +0200

    First commit

---
+++ iframe-test/js/data.js
@@ -0,0 +1,60 @@
+{
+	"top_logo" : "Eksperten",
+	"top_media_text" : "Mediehuset Nettavisen",
+	"top_text": "i samarbeid med Norsk Rikstoto",
+	"expert_text" : "Mediehuset Nettavisen har en rekke eksperter i sin stall. Roger Sundvisson (V64/V4 Sverige), ), Kurt Leirvåg (V64/V4 Sverige), i tillegg fast mann på Bergen travpark, Leangen og Forus og øvrig norsk trav), Håkon Einar Hansen (alle norske baner, V65 Bjerke og V75 Norge), Birger Christensen (galopp Norge og Sverige), Morten Olsen (galopp Norge og Sverige) og Carsten Rabjerg (Danmark) er våre eksperter.",
+	"expert_img" : "http://upload.uxpin.s3.amazonaws.com/files/16318/20478/ilu_ekspert2_BW.png",
+	"expert_url" ."",
+	"bottom_text" : "Se flere Experten-spill fra Mediehuset Nettavisen",
+	"bottom_url" : "",
+	"spill": [
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		},
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		},
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		},
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		},
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		},
+		{
+			"type": "V5A",
+			"heading1" : "Klosterskogen, Torsdag kl. 18:30",
+			"heading2" : "Leirvåg and Hansen 4",
+			"info" : "Torsdag har Nettavisens meget rutinerte duo Hansen og Leirvåg slått sine pjalter sammen til den doble V5-omgangen på Klosterskogen.",
+			"andeler" : "1/5",
+			"pris" : "300,-"
+		}
+	]
+}
